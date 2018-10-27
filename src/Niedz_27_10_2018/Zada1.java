9616fc7420a7bc1098d43d793b7c399d991bb413
Stanisław Sieczkowski <s.sieczkowski9@gmail.com>
2018-10-27 09:49:55 +0200

    Mesage

---
+++ src/Niedz_27_10_2018/Zada1.java
@@ -0,0 +1,10 @@
+package Niedz_27_10_2018;
+
+public class Zada1 {
+    public static void main(String[] args) {
+        for (int i=0;i<10;i++){
+            char a = '*';
+            System.out.println(a);
+        }
+    }
+}
